b0abb7feafdaa5e5fa694b7e0789b03d9d51f2e9
hieutrinhds <hieutrinh.au@gmail.com>
2020-05-25 08:54:22 +0700

    update sraping follwing Coderscholl instructions

--- web_scraper.py
+++ web_scraper.py
@@ -2,14 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 
 # get the data
-data = requests.get(url)
+r = requests.get('https://vnexpress.net/')
 
 # load data into bs4
-soup = BeautifulSoup(data.text, 'html.parser')
+soup = BeautifulSoup(r.text, 'html.parser')
 
-
-
-
+# First occurence of the tag
+first_article = soup.find('article', {'class':'item-news'})
+# print(first_article.prettify())
+# All occurences of the tags
+articles = soup.find_all('article', {'class': 'item-news'})
+# print(articles)
+# First occurence of the tag w/o attribue
+first_p = soup.p
+# print(first_p)
 
 
 
